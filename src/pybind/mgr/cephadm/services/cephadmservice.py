aainscow/ceph

commit aae5ef14644481dc166dce7dca4784f372ddc0fd

mgr/cephadm: Update service name as userId for nfs cluster, to keep it same across nfs cluster
Resolves: rhbz#2365590
Signed-off-by: Shweta Bhosale <Shweta.Bhosale1@ibm.com>

src/pybind/mgr/cephadm/services/cephadmservice.py
@@ -63,7 +63,7 @@ def get_auth_entity(daemon_type: str, daemon_id: str, host: str = "") -> AuthEnt
     """
     # despite this mapping entity names to daemons, self.TYPE within
     # the CephService class refers to service types, not daemon types
-    if daemon_type in ['rgw', 'rbd-mirror', 'cephfs-mirror', 'nfs', "iscsi", 'nvmeof', 'ingress', 'ceph-exporter', 'crash']:
+    if daemon_type in ['rgw', 'rbd-mirror', 'cephfs-mirror', "iscsi", 'nvmeof', 'ingress', 'ceph-exporter']:
         return AuthEntity(f'client.{daemon_type}.{daemon_id}')
     elif daemon_type in ['agent', 'node-proxy']:
         if host == "":
@@ -908,9 +908,10 @@ class CephService(CephadmService):
 
     def post_remove(self, daemon: DaemonDescription, is_failed_deploy: bool) -> None:
         super().post_remove(daemon, is_failed_deploy=is_failed_deploy)
-        self.remove_keyring(daemon)
+        if daemon.daemon_type != 'nfs':
+            self.remove_keyring(daemon)
 
-    def get_auth_entity(self, daemon_id: str, host: str = "") -> AuthEntity:
+    def get_auth_entity(self, daemon_id: str, host: str = "", rados_user: str = '') -> AuthEntity:
         return get_auth_entity(self.TYPE, daemon_id, host=host)
 
     def get_config_and_keyring(self,

src/pybind/mgr/cephadm/migrations.py
@@ -118,6 +118,10 @@ class Migrations:
             if self.migrate_7_8():
                 self.set(8)
 
+        if self.mgr.migration_current == 8 and not startup:
+            if self.migrate_8_9():
+                self.set(9)
+
         if self.mgr.migration_current == 9:
             if self.migrate_9_10():
                 self.set(10)
@@ -486,6 +490,16 @@ class Migrations:
         if nfs_services:
             self.mgr.set_store('nfs_services_with_old_nodeid', ','.join(nfs_services))
         self.mgr.remove_health_warning('CEPHADM_MIGRATION_FAILURE')
+        return True
+
+    def migrate_8_9(self) -> bool:
+        # Store extisting NFS services to mon store, to use old userid for them
+        nfs_services = []
+        service_specs = self.mgr.spec_store.get_specs_by_type('nfs')
+        for service_name, spec in service_specs.items():
+            nfs_services.append(service_name)
+        if nfs_services:
+            self.mgr.set_store('nfs_services_with_old_userid', ','.join(nfs_services))
         return True
 
     def migrate_9_10(self) -> bool:

src/pybind/mgr/cephadm/module.py
@@ -3276,14 +3276,16 @@ Then run the following:
                 raise OrchestratorError(
                     f'If {service_name} is removed then the following OSDs will remain, --force to proceed anyway\n{msg}')
         elif service_name.startswith('nfs.'):
-            # check if its using old node id style and remove from mon store
-            nfs_services = self.get_store('nfs_services_with_old_nodeid')
-            if nfs_services:
-                nfs_services = nfs_services.split(',')
-                if service_name in nfs_services:
-                    nfs_services.remove(service_name)
-                    val = ','.join(nfs_services) if nfs_services else None
-                    self.set_store('nfs_services_with_old_nodeid', val)
+            # check if its using old node id style or user id and remove from mon store
+            mon_keys = ['nfs_services_with_old_nodeid', 'nfs_services_with_old_userid']
+            for key in mon_keys:
+                nfs_services = self.get_store(key)
+                if nfs_services:
+                    nfs_services = nfs_services.split(',')
+                    if service_name in nfs_services:
+                        nfs_services.remove(service_name)
+                        val = ','.join(nfs_services) if nfs_services else None
+                        self.set_store(key, val)
 
         found = self.spec_store.rm(service_name)
         if found and service_name.startswith('osd.'):

src/pybind/mgr/cephadm/services/nfs.py
@@ -36,11 +36,20 @@ class NFSService(CephService):
     def ranked(self, spec: ServiceSpec) -> bool:
         return True
 
-    def fence(self, daemon_id: str) -> None:
+    def fence(self, service_name: str, daemon_id: str) -> None:
         logger.info(f'Fencing old nfs.{daemon_id}')
+        rados_user = self.get_daemon_user(service_name, daemon_id)
+        entity = self.get_auth_entity(daemon_id, rados_user=rados_user)
+
+        # as new auth entity is common across cluster, we will need to delete it for last daemon
+        if rados_user == service_name and self.mgr.cache.get_daemons_by_service(service_name):
+            logger.debug(f'Not removing key for {entity} as daemons still exists')
+            return
+
+        logger.info(f'Removing key for {entity}')
         ret, out, err = self.mgr.mon_command({
             'prefix': 'auth rm',
-            'entity': f'client.nfs.{daemon_id}',
+            'entity': entity,
         })
 
         # TODO: block/fence this entity (in case it is still running somewhere)
@@ -53,7 +62,7 @@ class NFSService(CephService):
             if rank >= num_ranks:
                 for daemon_id in m.values():
                     if daemon_id is not None:
-                        self.fence(daemon_id)
+                        self.fence(spec.service_name(), daemon_id)
                 del rank_map[rank]
                 nodeid = self.get_daemon_nodeid(spec.service_name(), rank)
                 self.mgr.log.info(f'Removing {nodeid} from the ganesha grace table')
@@ -64,7 +73,7 @@ class NFSService(CephService):
                 for gen, daemon_id in list(m.items()):
                     if gen < max_gen:
                         if daemon_id is not None:
-                            self.fence(daemon_id)
+                            self.fence(spec.service_name(), daemon_id)
                         del rank_map[rank][gen]
                         self.mgr.spec_store.save_rank_map(spec.service_name(), rank_map)
                         # Restart the existing NFS daemon for that rank, as clients will be unable to
@@ -82,6 +91,12 @@ class NFSService(CephService):
         if out and service_name in out.split(','):
             return f'{service_name}.{rank}'
         return str(rank)
+
+    def get_daemon_user(self, service_name: str, daemon_id: str) -> str:
+        out = self.mgr.get_store('nfs_services_with_old_userid')
+        if out and service_name in out.split(','):
+            return f'nfs.{daemon_id}'
+        return f'{service_name}'
 
     @classmethod
     def get_dependencies(
@@ -116,8 +131,8 @@ class NFSService(CephService):
         nfs_idmap_conf = '/etc/ganesha/idmap.conf'
 
         # create the RADOS recovery pool keyring
-        rados_user = f'{daemon_type}.{daemon_id}'
-        rados_keyring = self.create_keyring(daemon_spec)
+        rados_user = self.get_daemon_user(daemon_spec.service_name, daemon_id)
+        rados_keyring = self.create_keyring(daemon_spec, rados_user)
 
         # ensure rank is known to ganesha
         self.mgr.log.info(f'Ensuring {nodeid} is in the ganesha grace table')
@@ -130,7 +145,7 @@ class NFSService(CephService):
         monitoring_ip, monitoring_port = self.get_monitoring_details(daemon_spec.service_name, host)
 
         # create the RGW keyring
-        rgw_user = f'{rados_user}-rgw'
+        rgw_user = f'{daemon_type}.{daemon_id}-rgw'
         rgw_keyring = self.create_rgw_keyring(daemon_spec)
         bind_addr = ''
         if spec.virtual_ip and not spec.enable_haproxy_protocol:
@@ -259,10 +274,15 @@ class NFSService(CephService):
                 )
                 raise RuntimeError(result.stderr.decode("utf-8"))
 
-    def create_keyring(self, daemon_spec: CephadmDaemonDeploySpec) -> str:
+    def get_auth_entity(self, daemon_id: str, host: str = "", rados_user: str = '') -> AuthEntity:
+        if rados_user:
+            return AuthEntity(f'client.{rados_user}')
+        return AuthEntity(f'client.{self.TYPE}.{daemon_id}')
+
+    def create_keyring(self, daemon_spec: CephadmDaemonDeploySpec, rados_user: str) -> str:
         daemon_id = daemon_spec.daemon_id
         spec = cast(NFSServiceSpec, self.mgr.spec_store[daemon_spec.service_name].spec)
-        entity: AuthEntity = self.get_auth_entity(daemon_id)
+        entity: AuthEntity = self.get_auth_entity(daemon_id, rados_user=rados_user)
 
         osd_caps = 'allow rw pool=%s namespace=%s' % (POOL_NAME, spec.service_id)
 
@@ -342,9 +362,29 @@ class NFSService(CephService):
             'entity': entity,
         })
 
+    def remove_keyring(self, daemon: DaemonDescription) -> None:
+        assert daemon.daemon_id is not None
+        daemon_id: str = daemon.daemon_id
+        service_name = daemon.service_name()
+
+        rados_user = self.get_daemon_user(service_name, daemon_id)
+        entity = self.get_auth_entity(daemon_id, rados_user=rados_user)
+
+        # as new auth entity is common across cluster, we will need to delete it for last daemon
+        if rados_user == service_name and self.mgr.cache.get_daemons_by_service(service_name):
+            logger.debug(f'Not removing key for {entity} as daemons still exists')
+            return
+
+        logger.info(f'Removing key for {entity}')
+        ret, out, err = self.mgr.mon_command({
+            'prefix': 'auth rm',
+            'entity': entity,
+        })
+
     def post_remove(self, daemon: DaemonDescription, is_failed_deploy: bool) -> None:
         super().post_remove(daemon, is_failed_deploy=is_failed_deploy)
         self.remove_rgw_keyring(daemon)
+        self.remove_keyring(daemon)
 
     def ok_to_stop(self,
                    daemon_ids: List[str],

src/pybind/mgr/cephadm/services/smb.py
@@ -229,7 +229,7 @@ class SMBService(CephService):
         # TODO ???
         logger.warning('config_dashboard is a no-op')
 
-    def get_auth_entity(self, daemon_id: str, host: str = "") -> AuthEntity:
+    def get_auth_entity(self, daemon_id: str, host: str = "", rados_user: str = '') -> AuthEntity:
         # We want a clear, distinct auth entity for fetching the config versus
         # data path access.
         return AuthEntity(f'client.{self.TYPE}.config.{daemon_id}')

src/pybind/mgr/cephadm/tests/test_cephadm.py
@@ -1482,7 +1482,7 @@ class TestCephadm(object):
                 placement=PlacementSpec(hosts=[HostPlacementSpec('test', '', 'x')], count=1),
                 unmanaged=True)
             ),  # noqa: E124
-            ('client.nfs.x', True, ServiceSpec(
+            ('client.nfs.id', True, ServiceSpec(
                 service_type='nfs',
                 service_id='id',
                 placement=PlacementSpec(hosts=[HostPlacementSpec('test', '', 'x')], count=1),

src/pybind/mgr/cephadm/tests/test_services.py
@@ -3765,14 +3765,14 @@ def test_ingress_config_nfs_proxy_protocol(
         '}\n'
         '\n'
         'RADOS_KV {\n'
-        '        UserId = "nfs.foo.test.0.0";\n'
+        '        UserId = "nfs.foo";\n'
         '        nodeid = "0";\n'
         '        pool = ".nfs";\n'
         '        namespace = "foo";\n'
         '}\n'
         '\n'
         'RADOS_URLS {\n'
-        '        UserId = "nfs.foo.test.0.0";\n'
+        '        UserId = "nfs.foo";\n'
         '        watch_url = '
         '"rados://.nfs/foo/conf-nfs.foo";\n'
         '}\n'
@@ -3789,7 +3789,7 @@ def test_ingress_config_nfs_proxy_protocol(
         'config': '',
         'extra_args': ['-N', 'NIV_EVENT'],
         'keyring': (
-            '[client.nfs.foo.test.0.0]\n'
+            '[client.nfs.foo]\n'
             'key = None\n'
         ),
         'namespace': 'foo',
@@ -3802,7 +3802,7 @@ def test_ingress_config_nfs_proxy_protocol(
             ),
             'user': 'nfs.foo.test.0.0-rgw',
         },
-        'userid': 'nfs.foo.test.0.0',
+        'userid': 'nfs.foo',
     }
 
     nfs_daemons = [

commit 01819f9838374f9d9871e59176f110f033b77d07

cephadm: re-add 'crash' to daemon type list in get_auth_entity()
aae5ef14644481dc166dce7dca4784f372ddc0fd introduced a regression
most likely a merge conflict dropped crash daemon from the list by
mistake.

Resolves: ISCE-3931

Signed-off-by: Guillaume Abrioux <gabrioux@ibm.com>

src/pybind/mgr/cephadm/services/cephadmservice.py
@@ -63,7 +63,7 @@ def get_auth_entity(daemon_type: str, daemon_id: str, host: str = "") -> AuthEnt
     """
     # despite this mapping entity names to daemons, self.TYPE within
     # the CephService class refers to service types, not daemon types
-    if daemon_type in ['rgw', 'rbd-mirror', 'cephfs-mirror', "iscsi", 'nvmeof', 'ingress', 'ceph-exporter']:
+    if daemon_type in ['rgw', 'rbd-mirror', 'cephfs-mirror', "iscsi", 'nvmeof', 'ingress', 'ceph-exporter', 'crash']:
         return AuthEntity(f'client.{daemon_type}.{daemon_id}')
     elif daemon_type in ['agent', 'node-proxy']:
         if host == "":